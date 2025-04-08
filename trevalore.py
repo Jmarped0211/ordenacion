48cac779963b75e070ed1ab5145fbf6148a24d27
jmarped0211 <jmarped0211@g.educaand.es>
2025-04-08 10:08:17 +0200

    ordenacion

---
+++ trevalore.py
@@ -0,0 +1,10 @@
+n = []
+
+# Rutina para tomar datos del teclado y guardalos en la variable n de tipo lista
+for 1 in range(3):
+    n.append(int(inpu()))
+
+s = n 
+for i in range(2):   #Aqui se refleja el error p.ej. con [7, 5, 3]
+    if s[j] > s[j+1]:
+        
